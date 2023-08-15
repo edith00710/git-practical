d624a322ad8a3e44582d1f6517dd911ceaf92ad0
edith00710 <140491656+edith00710@users.noreply.github.com>
2023-08-15 20:23:20 +0930

    q17

--- question17.sh
+++ question17.sh
@@ -14,15 +14,14 @@ git add dir3
 git commit -m"dir3"
 git checkout branch1
 touch newfile1
-mv dir1/dir2/foo dir1/foo
-cd dir1
-rm -r dir2
-git commit -am"foo"
-cd ..
 git add newfile1
 git commit -m"newfile1"
-git add dir1/
-git commit -m"dir1/"
+cd dir1
+mv dir2/foo foo
+rm -r dir2
+git add .
+git commit -m"dir2/foo"
+cd ..
 git checkout main
 touch dir3/bar_copy
 git add dir3/bar_copy
