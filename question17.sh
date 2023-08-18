85b94344476641e3d58f19a8b4e7ea5834473896
edith00710 <140491656+edith00710@users.noreply.github.com>
2023-08-18 11:34:02 +0930

    17newly

--- question17.sh
+++ question17.sh
@@ -2,15 +2,17 @@ cd git-practice-03
 git branch branch1
 git checkout -b branch2
 rm dir3/bar
-mv dir3 dir1/dir3
+mv dir3 dir1/
 touch dir1/dir3/newfile2
-git -am ."newfile2"
+git add dir3
+git commit -m"dir3/"
 cd dir1/dir2
 git mv foo foo_modified
 git commit -am"foo_modified"
 cd ..
 cd ..
 git commit -am"newfile2"
+
 git checkout branch1
 touch newfile1
 git add newfile1
@@ -21,10 +23,11 @@ rm -r dir2
 git add .
 git commit -m"dir2/foo"
 cd ..
+
 git checkout main
-touch dir3/bar_copy
+cp dir3/bar dir3/bar_copy
 git add dir3/bar_copy
-git commit -m"bar_copy"
+git commit -m"add bar_copy"
 
 
 
